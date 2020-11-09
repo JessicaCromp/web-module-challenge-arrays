838011af671e3581ccfe2cdea37205016abe58ec
JessicaCromp <jessica.cromp@gmail.com>
2020-11-08 21:16:43 -0700

    second attempt

--- index.js
+++ index.js
@@ -100,9 +100,11 @@ Use the removeLastFlavor function below to do the following:
     For example: running removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]
 */
 
-function removeLastFlavor(/*your code here*/){
-   /*your code here*/
+function removeLastFlavor(array,index){
+   originalFlavors.pop[originalFlavors];
+   return array;
 }
+
 
 
 
